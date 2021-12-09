2dcc23bd7c0ff92bbd144c9c4616d546e5602f65
Rahul Gupta <rahul26021999@gmail.com>
2021-12-10 01:30:12 +0530

    callback 2

--- callback/callback.js
+++ callback/callback.js
@@ -4,10 +4,28 @@ const reviews = require('./reviews.json');
 
 const getMovie = (id, cb) => {
     setTimeout(() => {
-        const movie = movies.find((movie) => movie.id === id);
+        const movie = movies.find(movie => movie.id === id);
         cb(movie);
     }, 1000);
 };
+const getReview = (movieId, cb) => {
+    setTimeout(() => {
+        const review = reviews.find(review => review.movie_id === movieId);
+        cb(review);
+    }, 1000);
+};
+const getUser = (name, cb) => {
+    setTimeout(() => {
+        const user = users.find(user => user.name === name);
+        cb(user);
+    }, 1000);
+};
 getMovie(3, (movie) => {
+    getReview(movie.id, (review) => {
+        getUser(review.reviewer, (user) => {
+            console.log(user);
+        });
+        console.log(review);
+    });
     console.log(movie);
 });
